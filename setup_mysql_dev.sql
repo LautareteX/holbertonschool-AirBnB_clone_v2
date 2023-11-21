c55ac6cd3a3332fe05e72f5fe95070ccf9cce8ee
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-11-21 07:16:29 -0800

    sql scripts

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- Script that prepares a MySQL server for the project:
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;TIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
