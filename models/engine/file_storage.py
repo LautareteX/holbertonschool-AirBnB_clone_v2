7ab6cae7aab77e922bc2e8987771de57b42a1871
jotalgs <jose.pgilsuarez@gmail.com>
2023-11-23 23:53:19 +0000

    fs 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,7 +59,7 @@ class FileStorage:
     def delete(self, obj=None):
         """ delete a specified object asnjdhbksdnhsdksddnkdsjk"""
         if obj is not None:
-            rmkey = obj.__class__.__name__  + '.' + obj.id
+            rmkey = obj.__class__.__name__ + '.' + obj.id
             if rmkey in self.__objects:
-                del(self.__objects[rmkey])
+                del self.__objects[rmkey]
                 self.save()
