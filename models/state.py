f9c8af1db68b2d987017e8a00dae4061cbc94c56
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-11-24 06:48:17 -0800

    Some advance for task 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref="state", cascade='all, delete-orphan')
+
+    @property
+    def cities(self):
+        from models import storage
+        c_instances = storage.all("City").values()
+        m_cities = []
+        for i in c_instances:
+            if i.state_id == self.id:
+                m_cities.append(i)
+        return m_cities
