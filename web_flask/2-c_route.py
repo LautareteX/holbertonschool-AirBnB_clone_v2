fee0bb039348b05cb5ca0cc3a8c868cb76b108dc
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-12-16 11:56:05 -0800

    task 2 - 3

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"script that starts a Flask web application"
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_clone():
+    "script that starts a Flask web application"
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    "script that starts a Flask web application"
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    "script that starts a Flask web application"
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
