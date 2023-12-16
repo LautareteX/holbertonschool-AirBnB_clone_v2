ea51ebca7e9504b1ca1697833ac701370c473b2c
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-12-16 06:02:35 -0800

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"first flask ksnkdnskdsa"
+"script that starts a Flask web application"
 from flask import Flask
 
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_clone():
-    "blablabalblabalsvv ddffldlf slfdslf ldf "
+    "script that starts a Flask web application"
     return 'Hello HBNB!'
 
 if __name__ == "__main__":
