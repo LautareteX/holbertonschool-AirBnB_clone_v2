2b915e3d382a4b668e47dc2c6a5c88b72cbc4da8
LautareteX <lautarorodriguezcoitinho@gmail.com>
2023-12-17 15:00:22 -0800

    task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"script that starts a Flask web application"
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_clone():
+    "script that starts a Flask web application"
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    "script that starts a Flask web application"
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    "script that starts a Flask web application"
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    "script that starts a Flask web application"
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    "script that starts a Flask web application"
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def html_template(n):
+    "script that starts a Flask web application"
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def odd_or_even(n):
+    "script that starts a Flask web application"
+    if n % 2:
+        output = "odd"
+    else:
+        output = "even"
+    return render_template('6-number_odd_or_even.html', n=n, output=output)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
